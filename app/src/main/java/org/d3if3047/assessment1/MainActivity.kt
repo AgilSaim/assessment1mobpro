e99a7c566566774e2d15c360eff83bda04bae972
Agil Saim Setiawan <agilsaimsetiawan23@gmail.com>
2023-04-02 00:28:30 +0700

    Menambahkan view binding

--- app/src/main/java/org/d3if3047/assessment1/MainActivity.kt
+++ app/src/main/java/org/d3if3047/assessment1/MainActivity.kt
@@ -2,13 +2,21 @@ package org.d3if3047.assessment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import org.d3if3047.assessment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 
-    fun hitung(view: View) {}
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener { hitungDiskon() }
+    }
+    private fun hitungDiskon() {
+        Log.d("MainActivity", "Tombol diklik!")
+    }
 }
